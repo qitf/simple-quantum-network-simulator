4ed583449d36bad1ad14d93e3de00b210572c120
Sakumatcho <sakumadaisuke32@gmail.com>
2024-06-20 18:26:08 +0900

    feat implement QNode.send and QNode.receive

--- src/simple_quantum_network_simulator/base/qnode.py
+++ src/simple_quantum_network_simulator/base/qnode.py
@@ -1,4 +1,6 @@
+from struct import pack
 from simple_quantum_network_simulator.address import QNodeAddress
+from simple_quantum_network_simulator.packet import Packet
 from simple_quantum_network_simulator.qnic import QNic
 import simpy
 
@@ -15,8 +17,25 @@ class QNode:
         self.env = env
         self.address = address
 
-    def send(self, message: str) -> None:
-        pass
+    def send(self, message: str, to: QNodeAddress) -> Packet:
+        data = {
+            'message': message,
+            'to': to
+        }
+        return Packet(data=data, prev_hop=self.address)
+
+    def receive(self, packet: Packet) -> None:
+        if 'to' in packet.data.keys():  # dataの型が任意だから.keys()でいいのか？
+            if packet.data['to'] == self.address:
+                message = packet.data['message']
+                print(f'from: {packet.prev_hop}, message: {message}')
+            else:
+                print(f'Invalid address: {packet.data["to"]}')
+        else:
+            message = packet.data['message']
+            print(f'from: {packet.prev_hop}, message: {message}')
+
+
 
     def __repr__(self) -> str:
         return f"QNode({self.address.short_repr})"
@@ -25,7 +44,12 @@ class QNode:
 if __name__ == "__main__":
     env = simpy.Environment()
     qnode1 = QNode(env, QNodeAddress(1))
-    qnode1.send("Hello, world!")
+    message = qnode1.send("Hello, world!", QNodeAddress(2))
+    """
+    receiveの方にpacketがいるが、sendの時点でpacketができて
+    指定したアドレスのqnodeに向かってnetworkが運んでいくイメージ
+    ただ、それを今書くのはすごく長くなりそうな気がするので
+    """
 
     qnode2 = QNode(env, QNodeAddress(2))
-    packet = qnode2.receive()
+    qnode2.receive(message)
